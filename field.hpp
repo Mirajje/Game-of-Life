890c712c670b3235ab7ab165b0b9f6ee465513bc
Mirajje <vova.tarasov.7777@gmail.com>
2024-04-01 19:48:11 +0300

    some improvements

--- field.hpp
+++ field.hpp
@@ -1,25 +1,53 @@
 #pragma once
 
+#include <thread-pool.hpp>
+
 #include <array>
 #include <functional>
+
+inline constexpr std::size_t kThreadsAmount = 2;
 
 template <std::size_t N, std::size_t M>
 class Field {
   public:
-    Field(std::function<bool(const Field<N, M>&, std::size_t y, std::size_t x)>&& f)
+    Field(const std::function<bool(const std::array<std::array<bool, M>, N>&, std::size_t y, std::size_t x)>& f)
       : rule(f) {}
+
+    Field<N, M>& operator=(std::array<std::array<bool, M>, N>&& field) {
+      field_ = std::move(field);
+      
+      return *this;
+    }
 
     void AddCell(std::size_t y, std::size_t x) {
       field_[y][x] = true;
     }
 
-    void MoveOneStep() {
+    void RemoveCell(std::size_t y, std::size_t x) {
+      field_[y][x] = false;
+    }
+
+    std::array<std::array<bool, M>, N> MoveOneStep(ThreadPool& tp) {
       std::array<std::array<bool, M>, N> new_field;
-      for (std::size_t i = 0; i < N; ++i)
-        for (std::size_t j = 0; j < M; ++j) {
-          new_field[i][j] = rule(*this, i, j); 
-        }
-      field_ = std::move(new_field);
+      std::vector<Data> results;
+      results.reserve(kThreadsAmount);
+
+      for (std::size_t thread_num = 0; thread_num < kThreadsAmount; ++thread_num) {
+        results.push_back(tp.AddTasks({{[&new_field, this](std::size_t i){
+          std::size_t until = i + N / kThreadsAmount;
+
+          for (; i < until; ++i) {
+            for (std::size_t j = 0; j < M; ++j) {
+              new_field[i][j] = rule(field_, i, j);
+            }
+          }
+        }, thread_num * N / kThreadsAmount}}));
+      }
+
+      for (auto& result : results)
+        result.Get();
+
+      return new_field;
     }
 
     bool Get(std::size_t y, std::size_t x) const {
@@ -27,6 +55,6 @@ class Field {
     }
 
   private:
-    std::function<bool(const Field<N, M>&, std::size_t y, std::size_t x)> rule;
+    std::function<bool(const std::array<std::array<bool, M>, N>&, std::size_t y, std::size_t x)> rule;
     std::array<std::array<bool, M>, N> field_;
 };
--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include "field.hpp"
 
-#include <thread>
-
 #include <thread-pool.hpp>
 
 #include <GL/freeglut_std.h>
@@ -10,7 +8,7 @@
 
 constexpr std::size_t kWindowHeight = 1000; 
 constexpr std::size_t kWindowWidth = 1000;
-constexpr std::size_t kPointSize = 50.0;
+constexpr std::size_t kPointSize = 2;
 
 static_assert(kWindowHeight != 0 && kWindowWidth != 0 && kPointSize != 0);
 static_assert(kWindowHeight % kPointSize == 0 && kWindowWidth % kPointSize == 0);
@@ -18,44 +16,112 @@ static_assert(kWindowHeight % kPointSize == 0 && kWindowWidth % kPointSize == 0)
 constexpr std::size_t kHeight = kWindowHeight / kPointSize;
 constexpr std::size_t kWidth = kWindowWidth / kPointSize;
 
-Field<kHeight, kWidth> field([](const Field<kHeight, kWidth>& field, std::size_t y, std::size_t x) {
-  return field.Get(y, x) && y + x < 5;
-});
+static_assert(kHeight % kThreadsAmount == 0);
 
-void DrawField() {
-  using namespace std::chrono_literals;
+ThreadPool tp;
 
-  for (;;) {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POINTS);
+constexpr auto day_and_night = [](const std::array<std::array<bool, kWidth>, kHeight>& field, std::size_t y, std::size_t x) {
+  std::size_t sum = 0;
 
-    for (std::size_t i = 0; i < kHeight; ++i) {
-      for (std::size_t j = 0; j < kWidth; ++j) {
-        if (field.Get(i, j)) {
-          glVertex2d((float) j / kWidth, (float) i / kHeight);
-        }
+  for (std::size_t i = y - 1; i <= y + 1; ++i) {
+    for (std::size_t j = x - 1; j <= x + 1; ++j) {
+      if (i == y && j == x)
+        continue;
+
+      if (i >= 0 && j >= 0 && i < kHeight && j < kWidth && field.at(i).at(j)) {
+        ++sum;
       }
     }
-
-    glEnd();
-    glFlush();
-    field.MoveOneStep();
-    std::this_thread::sleep_for(1s);
   }
+
+  return sum == 3 || sum == 6 || sum == 7 || sum == 8 || (field.at(y).at(x) && sum == 4);
+};
+
+constexpr auto game_of_life = [](const std::array<std::array<bool, kWidth>, kHeight>& field, std::size_t y, std::size_t x) {
+  std::size_t sum = 0;
+
+  for (std::size_t i = y - 1; i <= y + 1; ++i) {
+    for (std::size_t j = x - 1; j <= x + 1; ++j) {
+      if (i == y && j == x)
+        continue;
+
+      if (i >= 0 && j >= 0 && i < kHeight && j < kWidth && field.at(i).at(j)) {
+        ++sum;
+      }
+    }
+  }
+
+  return sum == 3 || (field.at(y).at(x) && sum == 2);
+};
+
+Field<kHeight, kWidth> field(game_of_life);
+
+void Render() {
+  glClear(GL_COLOR_BUFFER_BIT);
+  glBegin(GL_POINTS);
+
+  for (std::size_t i = 0; i < kHeight; ++i) {
+    for (std::size_t j = 0; j < kWidth; ++j) {
+      if (field.Get(i, j)) {
+        glVertex2d(2 * static_cast<float>(j) / kWidth - 1, 2 * static_cast<float>(i) / kHeight - 1);
+      }
+    }
+  }
+
+  glEnd();
+  glFlush();
+}
+
+void MainLoop() {
+  for (;;) {
+    auto result = tp.AddTasks({{&Field<kHeight, kWidth>::MoveOneStep, field, std::ref(tp)}});
+    Render();
+    field = std::any_cast<std::array<std::array<bool, kWidth>, kHeight>>(**result.Get()[0]);
+  }
+}
+
+void Setup() {
+  using namespace std::chrono_literals;
+
+  glutMouseFunc([](int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+      field.AddCell(kHeight - static_cast<float>(y) / kWindowHeight * kHeight, static_cast<float>(x) / kWindowWidth * kWidth);
+      Render();
+    }
+
+    if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+      field.RemoveCell(kHeight - static_cast<float>(y) / kWindowHeight * kHeight, static_cast<float>(x) / kWindowWidth * kWidth);
+      Render();
+    }
+  });
+
+  glutKeyboardFunc([](unsigned char code, int x, int y){
+    switch (code) {
+      case 114: {
+        for (int i = 0; i < kHeight; ++i)
+          for (int j = 0; j < kWidth; ++j)
+            if (rand() % 2)
+              field.AddCell(i, j);
+      }
+      case 13: {
+        glutMouseFunc(NULL);
+        MainLoop();
+        break;
+      }
+    }
+  });
 }
  
 int main(int argc, char** argv) {
+  tp.ChangeWorkersAmount(kThreadsAmount);
+
   glutInit(&argc, argv);
   glutInitWindowSize(kWindowWidth, kWindowHeight);
   glutCreateWindow("Game of Life");
 
-  for (int i = 0; i < 20; ++i)
-    field.AddCell(0, i);
-
   glPointSize(kPointSize);
-
-  glutDisplayFunc(DrawField);
+    
+  glutDisplayFunc(Setup);
 
   glutMainLoop();
-  return 0;
 }
